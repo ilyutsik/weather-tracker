728ed2b6fcf6c69ab793ca15eb86f66de0910df5
Ilyutsik Andrei <ilyutsik.andrei@gmail.com>
2025-06-27 21:46:35 +0300

    add user entity and liquidbase scripts

---
+++ src/main/java/org/ilyutsik/controller/UserController.java
@@ -0,0 +1,9 @@
+package org.ilyutsik.controller;
+
+import org.ilyutsik.repository.UserRepository;
+
+public class UserController {
+
+    private UserRepository userRepository;
+
+}
---
+++ src/main/java/org/ilyutsik/model/User.java
@@ -0,0 +1,19 @@
+package org.ilyutsik.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    @Column(unique = true, nullable = false)
+    private String password;
+}
